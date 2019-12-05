6c16a74d9fdd3991f926f6e79db5648465ee4fd4
Terminaether <terminaether@tutanota.com>
2019-12-05 13:52:04 +0000

    Initial code commit

---
+++ app/src/main/java/codes/terminaether/watchlist/MainActivity.kt
@@ -0,0 +1,18 @@
+package codes.terminaether.watchlist
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * TODO (Documentation): Add file description.
+ *
+ * Created by terminaether on 2019-12-05.
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
