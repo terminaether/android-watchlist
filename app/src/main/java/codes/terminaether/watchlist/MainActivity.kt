1078cfa54bf5dabb8e50744799fc112b04e68f87
Terminaether <terminaether@tutanota.com>
2019-12-20 11:38:39 +0000

    Add working code for bottom navigation

--- app/src/main/java/codes/terminaether/watchlist/MainActivity.kt
+++ app/src/main/java/codes/terminaether/watchlist/MainActivity.kt
@@ -2,6 +2,11 @@ package codes.terminaether.watchlist
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import codes.terminaether.watchlist.feature.explore.ui.ExploreFragment
+import codes.terminaether.watchlist.feature.saved.ui.SavedFragment
+import codes.terminaether.watchlist.feature.trailers.ui.TrailersFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 /**
  * TODO (Documentation): Add file description.
@@ -13,6 +18,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadFragment(ExploreFragment())
+        bottom_nav.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.explore -> {
+                    loadFragment(ExploreFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+
+                R.id.saved -> {
+                    loadFragment(SavedFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+
+                R.id.trailers -> {
+                    loadFragment(TrailersFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+            }
+
+            false
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, fragment)
+        transaction.addToBackStack(fragment.tag)
+        transaction.commit()
     }
 
 }
