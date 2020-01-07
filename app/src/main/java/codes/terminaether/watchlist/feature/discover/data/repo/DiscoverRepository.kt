5a179e79892c29fff6c95c67d035b59ff32b9afe
Terminaether <terminaether@tutanota.com>
2020-01-07 14:38:12 +0000

    Remove constructor injection from DiscoverRepository

--- app/src/main/java/codes/terminaether/watchlist/feature/discover/data/repo/DiscoverRepository.kt
+++ app/src/main/java/codes/terminaether/watchlist/feature/discover/data/repo/DiscoverRepository.kt
@@ -1,18 +1,19 @@
 package codes.terminaether.watchlist.feature.discover.data.repo
 
+import codes.terminaether.watchlist.WatchlistApplication
 import codes.terminaether.watchlist.data.model.ApiResult
 import codes.terminaether.watchlist.data.repo.BaseRepository
 import codes.terminaether.watchlist.feature.discover.data.model.DiscoverMoviesResponse
-import codes.terminaether.watchlist.feature.discover.data.service.DiscoverService
-import javax.inject.Inject
 
 /**
  * Allows the app to use multiple data sources for the Discover feed.
  *
  * Created by terminaether on 2020-01-03.
  */
-class DiscoverRepository @Inject constructor(private val discoverService: DiscoverService) :
-    BaseRepository() {
+class DiscoverRepository : BaseRepository() {
+
+    private val discoverService =
+        WatchlistApplication.INSTANCE.networkComponent.getDiscoverService()
 
     suspend fun discoverMovies(): ApiResult<DiscoverMoviesResponse> {
         return safeApiCall(
