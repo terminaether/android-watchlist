6480bc61692a25b1f47a9822127d3c3e97a62eae
Terminaether <terminaether@tutanota.com>
2020-01-07 13:24:59 +0000

    Add inject constructor to DiscoverRepository

--- app/src/main/java/codes/terminaether/watchlist/feature/discover/data/repo/DiscoverRepository.kt
+++ app/src/main/java/codes/terminaether/watchlist/feature/discover/data/repo/DiscoverRepository.kt
@@ -4,13 +4,15 @@ import codes.terminaether.watchlist.data.model.ApiResult
 import codes.terminaether.watchlist.data.repo.BaseRepository
 import codes.terminaether.watchlist.feature.discover.data.model.DiscoverMoviesResponse
 import codes.terminaether.watchlist.feature.discover.data.service.DiscoverService
+import javax.inject.Inject
 
 /**
  * Allows the app to use multiple data sources for the Discover feed.
  *
  * Created by terminaether on 2020-01-03.
  */
-class DiscoverRepository(private val discoverService: DiscoverService) : BaseRepository() {
+class DiscoverRepository @Inject constructor(private val discoverService: DiscoverService) :
+    BaseRepository() {
 
     suspend fun discoverMovies(): ApiResult<DiscoverMoviesResponse> {
         return safeApiCall(
