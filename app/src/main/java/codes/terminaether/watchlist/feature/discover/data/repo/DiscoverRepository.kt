e8fb8b020b1aa5f387554993697c5c2bfa24e462
Terminaether <terminaether@tutanota.com>
2020-01-23 10:10:28 +0000

    Mark Media items that are saved locally before returning discover lists

--- app/src/main/java/codes/terminaether/watchlist/feature/discover/data/repo/DiscoverRepository.kt
+++ app/src/main/java/codes/terminaether/watchlist/feature/discover/data/repo/DiscoverRepository.kt
@@ -1,19 +1,22 @@
 package codes.terminaether.watchlist.feature.discover.data.repo
 
+import android.content.Context
 import codes.terminaether.watchlist.WatchlistApplication
 import codes.terminaether.watchlist.data.model.*
 import codes.terminaether.watchlist.data.repo.BaseRepository
+import codes.terminaether.watchlist.data.repo.SavedMediaRepository
 
 /**
  * Allows the app to use multiple data sources for the Discover feed.
  *
  * Created by terminaether on 2020-01-03.
  */
-class DiscoverRepository : BaseRepository() {
+class DiscoverRepository(private val context: Context) : BaseRepository() {
 
     private val discoverService =
         WatchlistApplication.INSTANCE.networkComponent.getDiscoverService()
 
+    //TODO (Code): Media returned should be observable
     suspend fun discoverMovies(): UiState<List<Media>> {
         val response = safeApiCall(
             call = { discoverService.discoverMovies().await() },
@@ -23,7 +26,8 @@ class DiscoverRepository : BaseRepository() {
         return when (response) {
             is ApiResult.Success -> {
                 val cleanDataSet = removeInvalidData(response.data.results)
-                UiState.Success(cleanDataSet)
+                val markedDataSet = markSavedData(cleanDataSet)
+                UiState.Success(markedDataSet)
             }
             is ApiResult.Error -> {
                 UiState.Error(response.exception)
@@ -40,7 +44,8 @@ class DiscoverRepository : BaseRepository() {
         return when (response) {
             is ApiResult.Success -> {
                 val cleanDataSet = removeInvalidData(response.data.results)
-                UiState.Success(cleanDataSet)
+                val markedDataSet = markSavedData(cleanDataSet)
+                UiState.Success(markedDataSet)
             }
             is ApiResult.Error -> {
                 UiState.Error(response.exception)
@@ -48,8 +53,27 @@ class DiscoverRepository : BaseRepository() {
         }
     }
 
+    /**
+     * Marks Media items that are saved locally.
+     * With thanks to Aaron.
+     */
+    private fun markSavedData(dataSet: List<Media>): List<Media> {
+        val savedIds = SavedMediaRepository(context).getMediaIds()
+        val iterator = dataSet.iterator()
+        iterator.forEach {
+            if (savedIds.contains(it.id)) it.isSaved = true
+        }
+
+        return dataSet
+    }
+
+    /**
+     * Removes Media items without a valid ID, Poster Path and Title/Name.
+     * With thanks to Conor.
+     */
     private fun removeInvalidData(dataSet: List<Media>): List<Media> {
         val cleanDataSet = dataSet.toMutableList()
+        cleanDataSet.removeAll { it.id == null }
         cleanDataSet.removeAll { it.posterPath.isNullOrBlank() }
 
         val iterator = cleanDataSet.iterator()
