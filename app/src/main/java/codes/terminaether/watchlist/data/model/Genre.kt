139eed7048d6e995131685b6ead7183472158cc0
Terminaether <terminaether@tutanota.com>
2020-01-28 15:58:11 +0000

    Cleanup old references in Genre model

--- app/src/main/java/codes/terminaether/watchlist/data/model/Genre.kt
+++ app/src/main/java/codes/terminaether/watchlist/data/model/Genre.kt
@@ -1,7 +1,7 @@
 package codes.terminaether.watchlist.data.model
 
 /**
- * A simple data class representing a Genre for a [Movie] or [Show].
+ * A simple data class representing a [Media] Genre.
  * @see [Genres](https://developers.themoviedb.org/3/genres).
  *
  * Created by terminaether on 2020-01-14.
