d861cf138361c5c0dbea3406b96e3f04fe2bc892
Terminaether <terminaether@tutanota.com>
2020-02-06 09:56:58 +0000

    Implement DiffUtil's getChangePayload method

--- app/src/main/java/codes/terminaether/watchlist/adapters/MediaListAdapter.kt
+++ app/src/main/java/codes/terminaether/watchlist/adapters/MediaListAdapter.kt
@@ -1,5 +1,6 @@
 package codes.terminaether.watchlist.adapters
 
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -23,6 +24,17 @@ import coil.api.load
 class MediaListAdapter(private val mediaSaveListener: MediaSaveListener) :
     ListAdapter<Media, MediaListAdapter.MediaViewHolder>(MediaDiffCallback()) {
 
+    companion object {
+        const val PAYLOAD_KEY_POSTER_PATH_CHANGED = "posterPath"
+        const val PAYLOAD_KEY_MEDIA_TITLE_CHANGED = "mediaTitle"
+        const val PAYLOAD_KEY_CONCISE_OVERVIEW_CHANGED = "conciseOverview"
+        const val PAYLOAD_KEY_SAVE_STATE_CHANGED = "hasSaveStateChanged"
+    }
+
+    interface MediaSaveListener {
+        fun onListItemSaveClick(media: Media)
+    }
+
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
@@ -37,16 +49,18 @@ class MediaListAdapter(private val mediaSaveListener: MediaSaveListener) :
     }
 
     override fun onBindViewHolder(holder: MediaViewHolder, position: Int) {
-        holder.bind(getItem(position))
+        onBindViewHolder(holder, position, mutableListOf())
     }
 
-    private class MediaDiffCallback : DiffUtil.ItemCallback<Media>() {
-        override fun areItemsTheSame(oldItem: Media, newItem: Media): Boolean {
-            return (oldItem.id == newItem.id)
-        }
-
-        override fun areContentsTheSame(oldItem: Media, newItem: Media): Boolean {
-            return (oldItem.describeDrawableContents() == newItem.describeDrawableContents())
+    override fun onBindViewHolder(
+        holder: MediaViewHolder,
+        position: Int,
+        payloads: MutableList<Any>
+    ) {
+        if (payloads.isEmpty()) {
+            holder.updateViewUi(getItem(position), mutableListOf())
+        } else {
+            holder.updateViewUi(getItem(position), payloads)
         }
     }
 
@@ -59,27 +73,46 @@ class MediaListAdapter(private val mediaSaveListener: MediaSaveListener) :
         private val overview: TextView = mediaView.findViewById(R.id.overview)
         private val save: ImageButton = mediaView.findViewById(R.id.save)
 
-        fun bind(media: Media) {
-            //TODO (UI): Dynamically set image size based on device size
-            //Populate the item's poster
-            poster.load("https://image.tmdb.org/t/p/w342" + media.posterPath)
+        fun updateViewUi(media: Media, payloads: MutableList<Any>) {
+            val redrawAll = payloads.isNullOrEmpty()
+            val payloadBundle = if (redrawAll) null else payloads[0] as Bundle
 
-            //Populate the item's title
-            title.text = media.mediaTitle
-
-            //Populate the item's overview via a shortened string, handle empty overviews
-            var conciseOverview = media.getConciseOverview()
-            if (conciseOverview.isBlank()) {
-                conciseOverview = context.getString(R.string.list_item_empty_overview)
+            if (redrawAll || payloadBundle!!.containsKey(PAYLOAD_KEY_POSTER_PATH_CHANGED)) {
+                //TODO (UI): Dynamically set image size based on device size
+                poster.load("https://image.tmdb.org/t/p/w342" + media.posterPath)
             }
-            overview.text = conciseOverview
 
-            //Indicate an item's presence in the local database
-            save.setOnClickListener(this)
-            if (media.isSaved) {
-                save.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.ic_save_filled))
-            } else {
-                save.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.ic_save_empty))
+            if (redrawAll || payloadBundle!!.containsKey(PAYLOAD_KEY_MEDIA_TITLE_CHANGED)) {
+                title.text = media.mediaTitle
+            }
+
+            if (redrawAll || payloadBundle!!.containsKey(PAYLOAD_KEY_CONCISE_OVERVIEW_CHANGED)) {
+                //Populate the item's overview via a shortened string, handle empty overviews
+                var conciseOverview = media.getConciseOverview()
+                if (conciseOverview.isBlank()) {
+                    conciseOverview = context.getString(R.string.list_item_empty_overview)
+                }
+                overview.text = conciseOverview
+            }
+
+            if (redrawAll || payloadBundle!!.containsKey(PAYLOAD_KEY_SAVE_STATE_CHANGED)) {
+                //Indicate an item's presence in the local database
+                save.setOnClickListener(this)
+                if (media.isSaved) {
+                    save.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            context,
+                            R.drawable.ic_save_filled
+                        )
+                    )
+                } else {
+                    save.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            context,
+                            R.drawable.ic_save_empty
+                        )
+                    )
+                }
             }
         }
 
@@ -89,8 +122,32 @@ class MediaListAdapter(private val mediaSaveListener: MediaSaveListener) :
         }
     }
 
-    interface MediaSaveListener {
-        fun onListItemSaveClick(media: Media)
+    private class MediaDiffCallback : DiffUtil.ItemCallback<Media>() {
+        override fun areItemsTheSame(oldItem: Media, newItem: Media): Boolean {
+            return (oldItem.id == newItem.id)
+        }
+
+        override fun areContentsTheSame(oldItem: Media, newItem: Media): Boolean {
+            return (oldItem.describeDrawableContents() == newItem.describeDrawableContents())
+        }
+
+        override fun getChangePayload(oldItem: Media, newItem: Media): Any? {
+            val bundle = Bundle()
+            if (oldItem.posterPath != newItem.posterPath) {
+                bundle.putBoolean(PAYLOAD_KEY_POSTER_PATH_CHANGED, true)
+            }
+            if (oldItem.mediaTitle != newItem.mediaTitle) {
+                bundle.putBoolean(PAYLOAD_KEY_MEDIA_TITLE_CHANGED, true)
+            }
+            if (oldItem.getConciseOverview() != newItem.getConciseOverview()) {
+                bundle.putBoolean(PAYLOAD_KEY_CONCISE_OVERVIEW_CHANGED, true)
+            }
+            if (oldItem.isSaved != newItem.isSaved) {
+                bundle.putBoolean(PAYLOAD_KEY_SAVE_STATE_CHANGED, true)
+            }
+
+            return bundle
+        }
     }
 
 }
